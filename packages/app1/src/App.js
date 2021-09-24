e38f25d4841892b1dad6be84eb4904985eba7b5f
Mahak Singh <mahak.singh@sendinblue.com>
2021-09-24 15:57:42 +0530

    feat: add new flow
    BREAKING CHANGE: drop support for node 14

--- packages/app1/src/App.js
+++ packages/app1/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       new breaking change
+      new breaking change
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
